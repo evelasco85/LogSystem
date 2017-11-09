eed7032de0e2c43eec30092fc0269f9e8a27539f
evelasco85 <enrico_velasco@hotmail.com>
2017-11-09 11:34:30 +0800

    Added log monitor queue

---
+++ LogManagement/ProducerConsumerLogQueue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace LogManagement
+{
+    public class ProducerConsumerLogQueue<TLogEntity> : IDisposable
+    {
+        private EventWaitHandle _waitHandle;
+        private Thread _worker;
+        private readonly object _lockerObject;
+        private Queue<TLogEntity> _logTasks;
+        private ILogMonitor<TLogEntity> _logMonitor;
+
+        public ProducerConsumerLogQueue(ILogMonitor<TLogEntity> logMonitor)
+        {
+            _waitHandle = new AutoResetEvent(false);
+            _worker = new Thread(Work);
+            _lockerObject = new object();
+            _logTasks = new Queue<TLogEntity>();
+            _logMonitor = logMonitor;
+
+            _worker.Start();
+        }
+
+        public void EnqueueLog(TLogEntity log)
+        {
+            lock (_lockerObject) _logTasks.Enqueue(log);
+
+            _waitHandle.Set();
+        }
+
+        public void Dispose()
+        {
+            EnqueueLog(default(TLogEntity));     // Signal the consumer to exit.
+            _worker.Join();         // Wait for the consumer's thread to finish.
+            _waitHandle.Close();            // Release any OS resources.
+        }
+
+        void Work()     //Run on separate thread
+        {
+            while (true)
+            {
+                TLogEntity log = default(TLogEntity);
+
+                lock (_lockerObject)
+                    if (_logTasks.Count > 0)
+                    {
+                        log = _logTasks.Dequeue();
+
+                        if (log == null) return;
+                    }
+
+                if (log != null)
+                {
+                    if (_logMonitor != null) _logMonitor.Evaluate(log);
+                }
+                else
+                    _waitHandle.WaitOne();         // No more tasks, block current thread - wait for a signal
+            }
+        }
+    }
+}
--- LogManagementTests/LogAnalyzerTests.cs
+++ LogManagementTests/LogAnalyzerTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using LogManagement;
 using LogManagement.Managers;
@@ -17,15 +19,65 @@ namespace LogManagementTests
         private ILogMonitor<ILogEntry> _logMonitor;
         private ILogManager _manager = LogManager.GetInstance();
         private string _invokedRuleId = string.Empty;
+        private ProducerConsumerLogQueue<ILogEntry> _logMonitorQueue;
 
         [TestInitialize]
         public void Initialize()
+        {
+            SetIdRetriever();
+            SetLogRepository();
+            SetLogInserter();
+            SetLogMonitor();
+            SetLogMonitorQueue();
+
+            _manager.LogEmit = log =>
+            {
+                if (log == null)
+                    return;
+
+                _logInserter.Insert(log);
+
+                //-->> Normalize log persistency table here (if necessary) prior to analysis of log entries
+                _logMonitor.Evaluate(log);
+                //-->> Clear log persistency table here (if necessary)
+            };
+        }
+
+        [TestCleanup]
+        public void MockedDestructor()
+        {
+            DestroyLogMonitorQueue();
+        }
+
+        void SetIdRetriever()
+        {
+            _manager.RetrieveBusinessTransactionId = () =>
+            {
+                return "123";
+            };
+
+            _manager.RetrieveSessionId = () =>
+            {
+                return "abc";
+            };
+
+            _manager.RetrieveUser = () =>
+            {
+                return "sa";
+            };
+        }
+
+        void SetLogRepository()
         {
             _logRepository = new LogRepository<ILogEntry>(new List<IBaseLogQueryObject<ILogEntry>>
             {
                 {new GetFailedInvocationLogsQuery(_inMemoryLogEntries)},
                 {new GetSuccessValidationDescriptionLogsQuery(_inMemoryLogEntries)},
             });
+        }
+
+        void SetLogInserter()
+        {
             _logInserter = new LogInserter<ILogEntry>(_logRepository,
                 (logEntityToAdd, preInsertRepository) =>
                 {
@@ -41,9 +93,13 @@ namespace LogManagementTests
                     /*Post-insert*/
                 }
             );
+        }
+
+        void SetLogMonitor()
+        {
             _logMonitor = new LogMonitor<ILogEntry>(_manager,
-                _logRepository,
-                new List<ILogTrigger<ILogEntry>>
+               _logRepository,
+               new List<ILogTrigger<ILogEntry>>
                 {
                     {
                         new LogTrigger<ILogEntry>("0001",
@@ -82,35 +138,30 @@ namespace LogManagementTests
                             })
                     },
                 }
-            );
-
-            _manager.RetrieveBusinessTransactionId = () =>
-            {
-                return "123";
-            };
-
-            _manager.RetrieveSessionId = () =>
-            {
-                return "abc";
-            };
-
-            _manager.RetrieveUser = () =>
-            {
-                return "sa";
-            };
-
-            _manager.LogEmit = log =>
-            {
-                if (log == null)
-                    return;
-
-                _logInserter.Insert(log);
-
-                //-->> Normalize log persistency table here (if necessary) prior to analysis of log entries
-                _logMonitor.Evaluate(log);
-                //-->> Clear log persistency table here (if necessary)
-            };
+           );
         }
+
+        #region Monitor Queue Intricacies
+        void SetLogMonitorQueue()
+        {
+            if (_logMonitorQueue == null) _logMonitorQueue = new ProducerConsumerLogQueue<ILogEntry>(_logMonitor);
+        }
+
+        void DestroyLogMonitorQueue()
+        {
+            if (_logMonitorQueue != null)
+            {
+                _logMonitorQueue.Dispose();
+
+                _logMonitorQueue = null;
+
+                string message = "Log monitor queue disposed";
+
+                Debug.WriteLine(message);
+                Console.WriteLine(message);
+            }
+        }
+        #endregion 
 
         [TestMethod]
         public void TestMethod1()
